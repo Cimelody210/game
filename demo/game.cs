e6998992a581e6f7574f19c32786d087265293e8
Nguyễn Tín <144228743+Cimelody210@users.noreply.github.com>
2024-03-12 14:38:39 +0700

    game

--- demo/game.cs
+++ demo/game.cs
@@ -1,2 +1,35 @@
+using System.Collection;
+using System.Collection.Generic;
 using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.UI;
 
+public class MainMenu: MonoBehaviour
+{
+    [SerializeField]
+    private GameObject mainMenu;
+    private GameObject optionMenu;
+    public Slider sound;
+    public Toddler hard;
+    public Toddler easy;
+    public Toddler medium;
+    public void Play(){
+        SceneManager.LoadScene("MyGame");
+    }
+    public void Exitime(){
+        Appication.Quit();
+    }
+    public void Option()
+    {
+        optionMenu.SetActive(true);
+        mainMenuMenu.SetActive(false);
+    }
+    public void Back()
+    {
+        string level ='';
+        optionMenu.SetActive(false);
+        mainMenuMenu.SetActive(true);
+        Debug.Log("No changes");
+    }
+    
+}
