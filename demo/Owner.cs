159d89089615358ce4c2e8c401eea2081e3c194c
Nguyễn Tín <144228743+Cimelody210@users.noreply.github.com>
2024-04-21 14:24:20 +0700

    gf

--- demo/Owner.cs
+++ demo/Owner.cs
@@ -13,6 +13,28 @@ public class Owner: MonoBeahviour
         }
         return 0f;
     }
+
+    void Spawn()
+    {
+        if(GameManager.instance != null && GameManager.instance.pool != null)
+        {
+            GameObject enemy = GameManager.instance.pool.Get(0);
+            if(enemy != null && spawnPoint.Length > 1)
+            {
+                enemy.transform.position  = spawnPoint[Random.Range(1, spawnPoint.Length)].position;
+                enemy.GetComponent<Enemy>().Init(spawnData[level]);
+            }
+        }
+    }
+    [System.SerializeField]
+    public class SpawnData
+    {
+        public float spamTime;
+        public float spriteType;
+        public int health;
+        public float speed;
+    }
+
     void Start()
     {
 
--- demo/demo1.cs
+++ demo/demo1.cs
@@ -8,7 +8,6 @@ public class LevelUp: MonoBeahviour
 {
   RecTransform rect;
   Item[] item;
-  
   void Awake()
   {
     rect = GetComponent<RecTransform>();
@@ -22,6 +21,29 @@ public class LevelUp: MonoBeahviour
     GameManager.instance.Stop();
     AudioManager.instance.PlaySfx(AudioManager.Sfx.LevelUp);
     AudioManager.instance.EffectBgm(true);
+  }
+  public void EffectBgm(bool isPlay)
+  {
+    bgmEffect.enabled =  isPlay;
+  }
+  public void PlaySfx(Sfx sfx)
+  {
+    for(int i = 0; i < sfxPlayers.length; i++)
+    {
+      int loopIndex  =(i + channelIndex) % sfxPlayers.Length;
+      if(sfxPlayers[loopIndex].isPlaying)
+        continue;
+      
+      int ranIndex  =0;
+      if(sfx == Sfx.Hit || sfx == Sfx.Melee)
+      {
+        ranIndex =  Random.Range(0, 2);
+      }
+      channelIndex  =loopIndex;
+      sfxPlayers[0].clip =  sfxClip[(int)sfx];
+      sfxPlayers[0].Play();
+      break;
+    }
   }
   
   public void Select()
