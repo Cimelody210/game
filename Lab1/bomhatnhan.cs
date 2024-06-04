ac8d58885a9a567a1b0043a91dea048211085970
Nguyễn Tín <144228743+Cimelody210@users.noreply.github.com>
2024-06-04 10:37:20 +0700

    Bom hạt nhân

---
+++ Lab1/bomhatnhan.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class NuclearExplosion : MonoBehaviour
+{
+    public ParticleSystem explosionParticles;
+    public Light explosionLight;
+
+    void Start()
+    {
+        // Kích hoạt Particle System và ánh sáng
+        explosionParticles.Play();
+        explosionLight.enabled = true;
+
+        // Tự động tắt hiệu ứng sau một khoảng thời gian
+        Invoke("TurnOffEffect", 3f);
+    }
+
+    void TurnOffEffect()
+    {
+        // Tắt Particle System và ánh sáng
+        explosionParticles.Stop();
+        explosionLight.enabled = false;
+
+        // Hủy đối tượng hiệu ứng sau một thời gian ngắn để giải phóng tài nguyên
+        Destroy(gameObject, 2f);
+    }
+}
