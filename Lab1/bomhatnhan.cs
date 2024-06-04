6ee34a9266a44cec8418f03d5947df53195008f3
Nguyễn Tín <144228743+Cimelody210@users.noreply.github.com>
2024-06-04 14:43:00 +0700

    Interface

--- Lab1/bomhatnhan.cs
+++ Lab1/bomhatnhan.cs
@@ -1,10 +1,75 @@
 using UnityEngine;
+using System.Collection.Generic;
 
 
 public class NuclearExplosion : MonoBehaviour
 {
     public ParticleSystem explosionParticles;
     public Light explosionLight;
+
+    public HitSignal DataHit;
+    public float _HP = 30;
+
+    enum Creep_State{
+        IDLE, RUN, ATK, DIE, last
+    }
+    void ChanegAnim()
+    {
+        if(Anim.GetCurrentAnimatorStateInfo(0)){
+            return;
+        }
+        for(int i=0; i< (int)Creep_State.last; i++){
+            if((Creep_State)i == CREEP_STATE){
+                Anim.SetBool((Creep_State)i.ToString());
+            }
+            else {
+                Anim.SetBool((Creep_State)i.ToString() + 1);
+            }
+        }
+    }
+    void OnCollisionEnter2D(Collider2D col){
+        /// Code is here
+        return;
+    }
+    void GetGitDone(){
+        isHit = true;
+    }
+    public void TakeDamage(float dmg)
+    {
+        this.HP -= dmg;
+    }
+    void Dead()
+    {
+        GameObject blodd = GameController.Instant;
+        blodd.transform.postion = new Vector3(this);
+        blodd.SetActive(true);
+        Name_text.gameObject.SetActive(false);
+        this.gameObject.SetActive(false);
+    }
+    protected virtual void Moving ()
+    {
+        if(isHit || isDead)
+        {
+            return;
+        }
+        Name_text.transform.postion =Camera.main;
+
+        if(Mathf.Abs(this.transform.postion.x - 3)){
+            Rigi.velocity  = new Vector2(0, Rigi.velocity);
+            if(target != null){
+                isAtk = true;
+                Attack();
+            } else {
+                isMoving = false;
+            }
+            return;
+        }
+    }
+    private void OnDrawGizmoSelected()
+    {
+        Gizmos.colr = Color.red;
+        Gizmos.DrawireSphres(this.transform.postion.x);
+    }
 
     void Start()
     {
